8a75edd7652868eb142aa19e0b90593d7c68ec3d
Gareth Jones <oxidising@gmail.com>
2021-03-16 11:21:48 +0000

    Tidied checkboxes and aligned

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,22 +1,26 @@
 import React from "react";
-// import "./Checkbox.css";
+import styles from "./Checkbox.module.css";
 
 class Checkbox extends React.Component {
   render() {
     return (
-      <label>
-        <span>{this.props.label}</span>
-        <input
-          name={this.props.name}
-          data-testid={this.props.name}
-          site={this.props.site}
-          species={this.props.species}
-          type="checkbox"
-          checked={this.props.checked}
-          onChange={this.props.onChange}
-          style={{ marginLeft: "0.5vw" }}
-        />
-      </label>
+      <div className={styles.main}>
+        <div className={styles.label}>
+            {this.props.label}
+        </div>
+        <div className={styles.box}>
+          <input
+            name={this.props.name}
+            data-testid={this.props.name}
+            site={this.props.site}
+            species={this.props.species}
+            type="checkbox"
+            checked={this.props.checked}
+            onChange={this.props.onChange}
+            style={{ marginLeft: "0.5vw" }}
+          />
+        </div>
+      </div>
     );
   }
 }
