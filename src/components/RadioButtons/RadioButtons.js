ce01bc17fe7f08207e72af63e3378f69479f489b
Gareth Jones <oxidising@gmail.com>
2021-08-18 16:37:16 +0100

    Adding radiobuttons correctly this time, update a couple of packages

---
+++ src/components/RadioButtons/RadioButtons.js
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styles from "./RadioButtons.module.css"
+
+class RadioButtons extends React.Component {
+  constructor() {
+    super();
+
+    this.onChangeValue = this.onChangeValue.bind(this);
+  }
+
+  onChangeValue(event) {
+    this.props.onChange(event.target.value);
+  }
+
+  render() {
+    const options = this.props.options;
+
+    let buttons = [];
+    for (const option of Object.keys(options)) {
+      const checked = option === this.props.selected;
+
+      const button = (
+        <label>
+          {option}
+          <input type="radio" key={option} value={option} checked={checked} onChange={this.handleChange} />
+        </label>
+      );
+
+      buttons.push(button);
+    }
+
+    return <div className={styles.buttons} onChange={this.onChangeValue}>{buttons}</div>;
+  }
+}
+
+RadioButtons.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.object.isRequired,
+  selected: PropTypes.string.isRequired
+}
+
+export default RadioButtons;
